380757f40919c1a8cb6f6ec7094253458c3353fd
githubmeking <130077291+githubmeking@users.noreply.github.com>
2024-11-05 22:22:02 +0300

    Update ba.py

--- ba.py
+++ ba.py
@@ -5,12 +5,13 @@ api_id = '28973855'
 api_hash = 'f5a97adb2ced0180e1fc2040ea9a0f05'
 client = TelegramClient('session_name', api_id, api_hash)
 
-# Kaynak ve hedef kanal kimliklerini veya kullanıcı adlarını girin, -100 önekini ekleyin
-source_channel = -1001626824569
-target_channel = -1002348222494
+# Kaynak ve hedef kanal kimliklerini girin
+source_channel = -1001626824569  # Mesajları alacağınız kanal
+target_channel = -1002255662739  # Mesajları ileteceğiniz kanal
 
 @client.on(events.NewMessage(chats=source_channel))
 async def forward_message(event):
+    # Mesajı hedef kanala iletin
     await client.send_message(target_channel, event.message)
 
 client.start()
